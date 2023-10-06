a013da5df55a61b04f0f0fdb5b2c284a0bb49d67
mausamtiwari <mausamtiwari93@gmail.com>
2023-10-06 16:36:06 +0200

    Third chapter: Classes Oefening1

---
+++ Chapter03_Classes/src/be/intecbrussel/Les1/Oefening1/MainCalc.java
@@ -0,0 +1,31 @@
+package be.intecbrussel.Les1.Oefening1;
+
+import java.util.Scanner;
+
+public class MainCalc {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        Scanner scanNum = new Scanner(System.in);
+
+        System.out.print("Please enter num1: ");
+        int num1 = scanNum.nextInt();
+        System.out.print("Please enter num2: ");
+        int num2 = scanNum.nextInt();
+        System.out.print("Please enter num3: ");
+        int num3 = scanNum.nextInt();
+
+        int smallNum = calculator.kleinsteNummer(num1, num2, num3);
+        System.out.println("The smallest number is: " + smallNum);
+
+        System.out.println(num1 + " -> " + calculator.EvenOdd(num1));
+
+        System.out.println(calculator.add(num1, num2));
+        System.out.println(calculator.subtract(num1, num2));
+        System.out.println(calculator.multiply(num1, num2));
+        System.out.println(calculator.divide(num1, num2));
+        System.out.println(calculator.mod(num1, num2));
+    }
+
+}

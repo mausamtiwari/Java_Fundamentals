41635c780a080f13257e16bc131c92857b9ff89d
mausamtiwari <mausamtiwari93@gmail.com>
2023-10-16 10:36:44 +0200

    Enum and Arrays Oefening5 edited.

--- Chapter04_ArraysAndEnums/src/be/intecbrussel/Oefening5/Oefening2.java
+++ Chapter04_ArraysAndEnums/src/be/intecbrussel/Oefening5/Oefening2.java
@@ -9,7 +9,14 @@ public class Oefening2 {
     public static void main(String[] args) {
         int[][] numbers = {{1, 2, 3}, {4, 5, 6}};
 
-        System.out.println(Arrays.deepToString(numbers));       // Prints the 2d array as string.
+        System.out.println(Arrays.deepToString(numbers));       // Prints string representation of 2d array.
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
